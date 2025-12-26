b294a5b04d3d771b7d13075ab6b7d357a1c5a19c
pavansrikar08 <pavansrikar95@gmail.com>
2025-12-26 14:15:17 +0530

    Add files via upload

---
+++ multiples of 5,even or odd.py
@@ -0,0 +1,18 @@
+#multiples of 5
+'''
+x=int(input())
+if x%5==0:
+    print("yes")
+else:
+        print("no")
+'''
+#even or odd
+'''
+x=int(input())
+if x%5==0:
+    print("yes")
+elif x%2==0:
+    print("even")
+else:
+    print("no")
+'''
