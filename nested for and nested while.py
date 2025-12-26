5218d5086b82d30527c63085728c49095fb5aa9d
pavansrikar08 <pavansrikar95@gmail.com>
2025-12-26 14:40:06 +0530

    Add files via upload

---
+++ nested for and nested while.py
@@ -0,0 +1,16 @@
+#nested for loop
+'''
+for r in range(4):
+    for c in range(5):
+        print("r=",r,"c=",c)
+    print()
+'''
+#nested while loop
+r=0
+while r<2:
+    c=0
+    while c<4:
+        print("r=",r,"c=",c)
+        c+=1
+        r+=1
+    print()
